4a3a27103e51256f87407ed67619449fffd6e5b7
Peagah <peagahvieira2003@gmail.com>
2022-08-29 09:29:33 -0300

    #

--- assets/php/blogin.php
+++ assets/php/blogin.php
@@ -1,6 +1,5 @@
 <?php
-$db_fail = 0;
-
+session_start();
 if(isset($_POST['email'],$_POST['password'])){
     $conexao = new PDO("mysql:host=localhost;dbname=formulario", "root","");
     $email = $_POST['email'];  
@@ -8,12 +7,29 @@ if(isset($_POST['email'],$_POST['password'])){
 
     $stm = $conexao->query("SELECT email FROM contato WHERE email='$email'");
     $stmt = $conexao->query("SELECT password FROM contato WHERE password='$password'");
-    $stmg = $conexao->query("SELECT fail FROM contato WHERE email='$email'");
     $db_email = $stm->fetch(PDO::FETCH_ASSOC);
     $db_password = $stmt->fetch(PDO::FETCH_ASSOC);
-    $db_fail = $stmg->fetch(PDO::FETCH_ASSOC);
 
-    if(isset($db_email['email']) == "$email" && isset($db_password['password']) == "$password"){
+    if(strlen("$password") < 7){
+        echo "<script type='text/javascript'>toastr.warning('Senha não pode ser menor do que <b>7</b> dígitos!', 'Situação', {
+            'closeButton': true,
+            'debug': false,
+            'newestOnTop': false,
+            'progressBar': true,
+            'positionClass': 'toast-bottom-left',
+            'preventDuplicates': false,
+            'onclick': null,
+            'showDuration': '300',
+            'hideDuration': '1000',
+            'timeOut': '5000',
+            'extendedTimeOut': '1000',
+            'showEasing': 'swing',
+            'hideEasing': 'linear',
+            'showMethod': 'fadeIn',
+            'hideMethod': 'fadeOut'
+        });</script>"; 
+    }
+    else if(isset($db_email['email']) == "$email" && isset($db_password['password']) == "$password"){
         header("Refresh: 5;url=https://www.google.com");
         echo "<script type='text/javascript'>toastr.success('Login efetuado, você será redirecionado em 5 segundos!', 'Situação', {
             'closeButton': true,
@@ -32,9 +48,8 @@ if(isset($_POST['email'],$_POST['password'])){
             'showMethod': 'fadeIn',
             'hideMethod': 'fadeOut'
         });</script>";
-        $stmf = $conexao->query("UPDATE contato SET fail = 0 WHERE email='$email'");
     }
-    else if($db_fail['fail'] == 5){
+    else if($_SESSION['count'] > 5){
         echo "<script type='text/javascript'>toastr.warning('Suas tentativas acabaram, você será bloqueado!', 'Situação', {
             'closeButton': true,
             'debug': false,
@@ -52,6 +67,19 @@ if(isset($_POST['email'],$_POST['password'])){
             'showMethod': 'fadeIn',
             'hideMethod': 'fadeOut'
         });</script>";
+        if(!isset($_SESSION['start'])){
+            $initial = new DateTime();
+            $_SESSION['start'] = $initial;
+            $_SESSION['end'] = $initial->add(new DateInterval('PT30S'));
+        }
+        else{
+            $now = new DateTime();
+            if($_SESSION['end'] <= $now){
+                $_SESSION['count'] = 0;
+                unset($_SESSION['start']);
+                unset($_SESSION['end']);
+            }
+        }
     }
     else if(isset($db_email['email']) !== "$email" or isset($db_password['password']) !== "$password"){
         echo "<script type='text/javascript'>toastr.error('E-mail ou senha incorreto!', 'Situação', {
@@ -71,6 +99,6 @@ if(isset($_POST['email'],$_POST['password'])){
             'showMethod': 'fadeIn',
             'hideMethod': 'fadeOut'
         });</script>";
-        $stmf = $conexao->query("UPDATE contato SET fail = fail+1 WHERE email='$email'");
+        $_SESSION['count'] ++;
     }
 }
